1cdadd85c62472718d007fd9aa5899c6b27b57b8
JToeps <jim.toepel@gmail.com>
2020-05-03 15:36:39 -0700

    Got a background and a zombie up and running.

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -11,5 +11,22 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
+        let centerScreen = CGPoint(x: size.width/2, y: size.height/2)
+        
+        //Add a background
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.position = centerScreen
+        background.zPosition = -1
+        addChild(background)
+        
+        //Add a zombie
+        let zombie1 = SKSpriteNode(imageNamed: "zombie1")
+        zombie1.position = CGPoint(x: 400, y:400)
+        addChild(zombie1)
+        
+        //Create a reference to the size of the BG node
+        let mySize = background.size
+        print("Size: \(mySize)")
+        
     }
 }
